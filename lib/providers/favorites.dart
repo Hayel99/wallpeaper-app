f95884fea20d7d94e190cd4b80ca1d8907d6be67
Hayel99 <hayelalhlayeel01@gmail.com>
2023-04-22 21:56:43 +0300

    my last update

--- lib/providers/favorites.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-
-import '../myphotos.dart';
-
-class Favorites extends ChangeNotifier {
-  notifyListeners();
-}
